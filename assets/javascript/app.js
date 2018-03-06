1461e7ec8d0f866918c9c171b01686b90e11ac35
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-03-06 15:22:54 -0500

    JS works for the most part

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,127 +41,243 @@
 /****************************************
               VARIABLES                      
 *****************************************/
-//ALLOWS THE PAGE TO LOAD BEFORE SCRIPT STARTS
-$(document).ready(function() {
-  questionsAndAnswers = [
-    {
-      Quest: "What is the capital of PA?",
-      answers: ["Harrisburg", "Augusta", "Jefferson City", "Sacramento"],
-      correctAns: 0
-    },
 
-    {
-      Quest: "What is the capital of PA?",
-      answers: ["Harrisburg", "Augusta", "Jefferson City", "Sacramento"],
-      correctAns: "Harrisburg"
+questionsAndAnswers = [
+
+  {
+    Quest: "Who was not in attendance at the Constitutional Convention?",
+    answers: ["George Washington", "Roger Sherman", "Thomas Jefferson", "Benjamin Franklin"],
+    correctAns: "Thomas Jefferson"
+  },
+
+  {
+    Quest: "What was the age of the youngest signer?",
+    answers: [" 36", "18", "26", " 31"],
+    correctAns: "26"
+  },
+
+  {
+    Quest: "What was the largest city (inhabitants) in the colonies at the time of the convention?",
+    answers: ["Williamsburg", "New York", "Boston", "Philadelphia"],
+    correctAns: "Philadelphia"
+  },
+
+  {
+    Quest: "Who were the only 2 Presidents who signed the Constitution??",
+    answers: ["Washington and John Adams", "Madison and Jefferson", "Adams and Madison", "Washington and Madison"],
+    correctAns: "Washington and Madison"
+  },
+
+  {
+    Quest: "Which word does NOT appear in the Constitution?",
+    answers: ["Democracy", "Confederacy", "Prince", "Pirates"],
+    correctAns: "Democracy"
+  },
+
+];
+
+var counter = 12;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unAnswered = 0;
+var htmlText;
+var timeRemaining;
+
+/***************************************************************************************************** */
+
+//CLICK TO START THE GAME
+//clicking starts the timer countdown and loads the question
+$(".timerPanel").html('<button type="button" class="btn btn-default">Click to Start</button>');
+
+//DEFINES what happens when THE CLICK to Start button is clicked
+$(".timerPanel").click(function () {
+
+
+  ///CLICK to end function
+  $(".clickToEndPanel").html('<button type="button" class="btn btn-default">Finished</button>');
+
+  //DEFINES what happens when THE CLICK to FINISHis clicked
+  $(".clickToEndPanel").click(function () {
+
+    finalTally();
+  })
+
+
+  //LOOP to cycle through all the questions
+  for (var i = 0; i < questionsAndAnswers.length; i++) {
+    //APPENDS the DOM with QUESTION
+    // console.log(i)
+    $(".mainPanel").append("<h2>" + questionsAndAnswers[i].Quest + "</h2>");
+
+    //LOOP to cycle through all the answers
+    for (var z = 0; z < questionsAndAnswers[i].answers.length; z++) {
+      // console.log(z);
+      //APPENDS the DOM with ANSWERS (loops the length of the questionAndAnswers array)
+      $(".mainPanel").append("<input type='radio'  name='ques-" + i + "'value='" + questionsAndAnswers[i].answers[z] + "'>" + questionsAndAnswers[i].answers[z] + "</input>");
     }
-  ];
-
-  var counter = 3;
-  var correctAnswers = 0;
-  var incorrectAnswers = 0;
-  var unAnswered = 0;
-  var htmlText;
-
-  //FUNTION TO START RUNNING GAME
-  function startGame() {
-    htmlText =
-      '<button type="button" class="btn btn-default">Click to Start</button>';
-    $(".timerPanel").html(htmlText);
-    console.log(htmlText);
   }
 
-  startGame();
+  countDown();
+});
 
-  //CLICK TO START THE GAME
-  //clicking starts the timer countdown and loads the question
-  $(".timerPanel").click(function() {
-    console.log("it works");
 
-    countDown();
 
-    // clickToEnd();
-    //THIS DOES NOT WORK
-    $(".clickToEndPanel").html(
-      '<button type="button" class="btn btn-default">Finished</button>'
-    );
-
-    //LOOP to cycle through all the questions
-    for (var i = 0; i < questionsAndAnswers.length; i++) {
-      //APPENDS the DOM with QUESTION
-      // console.log(i)
-      $(".mainPanel").append("<h2>" + questionsAndAnswers[i].Quest + "</h2>");
-
-      //LOOP to cycle through all the answers
-      for (var z = 0; z < questionsAndAnswers[i].answers.length; z++) {
-        // console.log(z);
-        //APPENDS the DOM with ANSWERS (loops the length of the questionAndAnswers array)
-        $(".mainPanel").append("<input type='radio' name='answer=' + i + 'value=' + z + '>'" + questionsAndAnswers[i].answers[z] + "</input>");
-
-          // “<input type=‘radio’ name=‘question-” + i + “’ value=’” +…
-        
-      }
-    }
-    //closes my click button for timer function
-  });
-
-  /****************************************
+/****************************************
                   FUNCTIONS                      
 *****************************************/
 
-  //the timer...write the timer to the DOM
-  function countDown() {
-    timeRemaining = setInterval(timerAmount, 1000);
+//FUNTION TO START RUNNING GAME
+function startGame() {
+  $(".timerPanel").html('<button type="button" class="btn btn-default">Click to Start</button>');
+  countDown();
+}
 
-    function timerAmount() {
-      if (counter > 0) {
-        counter--;
-        $(".timerPanel").html("<p>" + "Time Left: " + counter + "</p>");
-        console.log(counter);
-      } else {
-        endOfGame();
-      }
+//TIMER COUNTDOWN
+//the timer...write the timer to the DOM
+function countDown() {
+
+  timeRemaining = setInterval(timerAmount, 1000);
+
+
+  function timerAmount() {
+    if (counter > 0) {
+
+      $(".timerPanel").html("<p>" + "Time Left: " + counter + "</p>");
+      counter--;
+      // console.log(timeRemaining);
+      console.log(counter);
+
+    } else {
+      clearInterval(timeRemaining);
+
+      finalTally();
     }
   }
+};
 
-  //If timer expires
-  function endOfGame() {
-    htmlText = "<p>" + "Correct Answers: " + correctAnswers + "</p>";
-    $(".timerPanel").html(
-      "<p>" + "Correct Answers: " + correctAnswers + "</p>"
-    );
-    $(".mainPanel").hide();
-  }
 
-  //If user clicks to end game before timer expired
-  //THIS DOES NOT WORK
-  function clickToEnd() {
-    $(".clickToEndPanel").html(
-      '<button type="button" class="btn btn-default">Finished</button>'
-    );
-    // $('.clickToEndPanel').click(endOfGame());
+function startGame() {
+  $(".timerPanel").html('<button type="button" class="btn btn-default">Click to Start</button>');
+}
 
-    //onclick.....
-  }
 
-  function finalTally() {
-    for (var i = 0; i < questionsAndAnswers.length; i++) {
-      for (var z = 0; i < questionsAndAnswers.length; z++) {
-        if (
-          (questionsAndAnswers[i].correctAns =
-            questionsAndAnswers[i].answers[z])
-        ) {
-          correctAnswers++;
-        } else if (
-          questionsAndAnswers[i].correctAns != questionsAndAnswers[i].answers[z]
-        ) {
-          incorrectAnswers++;
-        } else {
-          unAnswered++;
-        }
-      }
+// FINAL TALLY
+//this computes the final stats
+function finalTally() {
+  clearInterval(timeRemaining);
+
+  $(".mainPanel").hide();
+  $(".clickToEndPanel").hide();
+  console.log(timeRemaining);
+
+
+  $.each($("input[name='ques-0']:checked"), function () {
+    console.log($("input[name='ques0']"));
+
+    if ($(this).val() === questionsAndAnswers[0].correctAns) {
+      (correctAnswers++);
+      console.log("a")
+      console.log(correctAnswers);
+
+    } else if ($(this).val() !== questionsAndAnswers[0].correctAns) {
+      incorrectAnswers++;
+      console.log("b")
+      console.log(incorrectAnswers);
+
+    } else {
+      console.log("x");
+      unAnswered++;
     }
-  }
+  })
 
-  //closes my document ready function
-});
+  $.each($("input[name='ques-1']:checked"), function () {
+    console.log($("input[name='ques1']"));
+
+    if ($(this).val() === questionsAndAnswers[1].correctAns) {
+      correctAnswers++;
+      console.log("a")
+      console.log(correctAnswers);
+
+    } else if ($(this).val() !== questionsAndAnswers[1].correctAns) {
+      incorrectAnswers++;
+      console.log("b")
+      console.log(incorrectAnswers);
+
+    } else {
+      console.log("x");
+      unAnswered++;
+    }
+  })
+
+  $.each($("input[name='ques-2']:checked"), function () {
+    console.log($("input[name='ques2']"));
+
+    if ($(this).val() === questionsAndAnswers[2].correctAns) {
+      correctAnswers++;
+      console.log("a")
+      console.log(correctAnswers);
+
+    } else if ($(this).val() !== questionsAndAnswers[2].correctAns) {
+      incorrectAnswers++;
+      console.log("b")
+      console.log(incorrectAnswers);
+
+    } else {
+      console.log("x");
+      unAnswered++;
+    }
+  })
+
+  $.each($("input[name='ques-3']:checked"), function () {
+    console.log($("input[name='ques3']"));
+
+    if ($(this).val() === questionsAndAnswers[3].correctAns) {
+      correctAnswers++;
+      console.log("a")
+      console.log(correctAnswers);
+
+    } else if ($(this).val() !== questionsAndAnswers[3].correctAns) {
+      incorrectAnswers++;
+      console.log("b")
+      console.log(incorrectAnswers);
+
+    } else {
+      console.log("x");
+      unAnswered++;
+    }
+  })
+
+  $.each($("input[name='ques-4']:checked"), function () {
+    console.log($("input[name='ques4']"));
+
+    if ($(this).val() === questionsAndAnswers[4].correctAns) {
+      correctAnswers++;
+      console.log("a")
+      console.log(correctAnswers);
+
+    } else if ($(this).val() !== questionsAndAnswers[4].correctAns) {
+      incorrectAnswers++;
+      console.log("b")
+      console.log(incorrectAnswers);
+
+    } else {
+      console.log("x");
+      unAnswered++;
+    }
+  })
+
+  resetGame();
+
+  $(".timerPanel").html(
+    "<p>" + "Correct Answers: " + correctAnswers + "</p>" + "<br />" + "<p>" + "Incorrect Answers: " + incorrectAnswers + "</p>" + "<br />" + "<p>" + "Unanswered Questions: " + unAnswered + "</p>" + "<br />"
+  );
+
+ 
+
+}
+
+
+function resetGame() {
+  $(".clickToEndPanel").html('<button type="button" class="btn btn-default">Click to reset Quiz</button>');
+  startGame();
+}
